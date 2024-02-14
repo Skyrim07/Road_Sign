c71479d49b20103df95ef4d3b7a87e8310cc034d
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2024-02-14 11:49:23 -0500

    Update Main.unity

--- Assets/Scripts/SceneObjects/Sign.cs
+++ Assets/Scripts/SceneObjects/Sign.cs
@@ -1,14 +1,29 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Sign : MonoBehaviour
 {
+    private SpriteRenderer spriteRend;
     public enum SignType
     {
         Stop
     }
+    public List<Sprite> sprites = new List<Sprite>();
     public SignType type;
 
+    private void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeSprite(SignType myType)
+    {
+        switch (myType)
+        {
+            case SignType.Stop:
+                spriteRend.sprite = sprites[0];
+                break;
+        }
+    }
     //will have index of different sign sprites that it assigns itself based on its type
 }
