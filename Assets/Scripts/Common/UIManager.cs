8dd23cd37413f55f28cd4d4268bf48f7cbd454a5
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2024-02-26 12:05:17 -0500

    factories

--- Assets/Scripts/Common/UIManager.cs
+++ Assets/Scripts/Common/UIManager.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class UIManager : SKMonoSingleton<UIManager> 
 {
+    [SerializeField] SKUIPanel colorFactoryPanel, shapeFactoryPanel;
     [SerializeField] SKUIPanel failPanel;
 
     public void SetState_FailPanel(bool active)
     {
         failPanel.SetState(active);
+    }
+    public void SetState_ColorFactoryPanel(bool active)
+    {
+        colorFactoryPanel.SetState(active);
+        if(active)
+        ColorFactory.instance.Init();
+    }
+    public void SetState_ShapeFactoryPanel(bool active)
+    {
+        shapeFactoryPanel.SetState(active);
+        if (active)
+            ShapeFactory.instance.Init();
     }
 }
--- Assets/Scripts/SceneObjects/PlayerMovement.cs
+++ Assets/Scripts/SceneObjects/PlayerMovement.cs
@@ -39,4 +39,6 @@ public class PlayerMovement : MonoBehaviour
             lose = true;
         }
     }
+
+
 }
