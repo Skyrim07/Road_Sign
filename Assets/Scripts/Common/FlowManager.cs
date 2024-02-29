4cd45af6cc885b4df31d3e1aac71369dcce28268
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2024-02-28 23:44:02 -0500

    quick fix

--- Assets/Scripts/Common/FlowManager.cs
+++ Assets/Scripts/Common/FlowManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static Unity.VisualScripting.Icons;
+
 
 public class FlowManager : SKMonoSingleton<FlowManager>
 {
