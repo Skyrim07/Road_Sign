d103e9d47358dea4127d3d121f1061da1cf3af20
jhdoubler01 <jhdoubler01@gmail.com>
2024-03-07 23:28:07 -0500

    more juicy player movement
    definitely still needs some tweaking and stuff but there is more feel to it now

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,25 +8,31 @@ public class PlayerMovement : MonoBehaviour
 
     public bool hitByCar;
     public bool lose;
+    public PlayerLogic playerLogic;
 
     private float speed = 3f;
-    [SerializeField] private float maxSpeed = 5f;
-    [SerializeField] private float accel = 0.5f;
+    [SerializeField] private float maxSpeed = 2f;
+    //[SerializeField] private float accel = 0.5f;
+    [SerializeField] private float moveForce = 5f;
+    [SerializeField] private float drag = 2f;
+    public AnimationCurve playerMovementCurve;
 
     [SerializeField] Animator anim;
 
     private Rigidbody2D rb;
     private Vector2 playerInput;
 
+    private bool stopMovement;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerLogic = GetComponent<PlayerLogic>();
     }
     private void Update()
     {
-        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        playerInput.Normalize();
+        
         anim.SetBool("Walk", playerInput.sqrMagnitude > 0);
 
         if(playerInput.sqrMagnitude > 0)
@@ -39,21 +45,51 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity += playerInput * (Mathf.Lerp(speed, maxSpeed, accel)) * RuntimeData.timeScale;
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, 0, maxSpeed), Mathf.Clamp(rb.velocity.y, 0, maxSpeed)).normalized;
+        float playerX = Input.GetAxisRaw("Horizontal");
+        float playerY = Input.GetAxisRaw("Vertical");
+
+        //disable diagonal movement
+        if (playerX != 0 && playerY != 0)
+        {
+            if (Mathf.Abs(playerX) > Mathf.Abs(playerY))
+                playerY = 0;
+            else
+                playerX = 0;
+        }
+
+        playerInput = new Vector2(playerX, playerY).normalized;
+
+        stopMovement = playerLogic.InFactory();
+        if(stopMovement) { playerInput = Vector2.zero; }
+
+
+        float moveCurve = playerMovementCurve.Evaluate(Time.time);
+
+        rb.AddForce(playerInput * moveForce * moveCurve, ForceMode2D.Force);
+
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        if(playerInput == Vector2.zero)
+        {
+            rb.velocity -= rb.velocity * drag * Time.fixedDeltaTime;
+        }
+
+        //rb.velocity = playerInput * (Mathf.Lerp(speed, maxSpeed, accel)) * RuntimeData.timeScale;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 normal = collision.contacts[0].normal;
+        Vector2 dir = Vector2.Reflect(rb.velocity.normalized, normal).normalized;
+        rb.velocity = dir * maxSpeed;
+
         if (collision.gameObject.CompareTag("Car"))
         {
             hitByCar = true;
             lose = true;
             SKAudioManager.instance.PlaySound("die");
-        }
-        else
-        {
-
         }
     }
 
