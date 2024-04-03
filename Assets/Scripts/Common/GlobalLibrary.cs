24359e4bb7af40637472d29f903ffdd4f0ee61a2
jhdoubler01 <jhdoubler01@gmail.com>
2024-04-02 23:11:22 -0400

    level 3 rough setup

--- Assets/Scripts/Common/GlobalLibrary.cs
+++ Assets/Scripts/Common/GlobalLibrary.cs
@@ -6,7 +6,7 @@ public class GlobalLibrary
 {
     public static readonly Dictionary<SceneCategory, SceneTitle[]> G_SCENE_CATEGORY_DICT = new Dictionary<SceneCategory, SceneTitle[]>
     {
-        {SceneCategory.All, new SceneTitle[]{SceneTitle.MainMenu, SceneTitle.Level1, SceneTitle.Level2} },
+        {SceneCategory.All, new SceneTitle[]{SceneTitle.MainMenu, SceneTitle.Level1, SceneTitle.Level2, SceneTitle.Level3, SceneTitle.Level4} },
     };
 
 }
@@ -19,4 +19,6 @@ public enum SceneTitle
     MainMenu,
     Level1,
     Level2,
+    Level3,
+    Level4,
 }
