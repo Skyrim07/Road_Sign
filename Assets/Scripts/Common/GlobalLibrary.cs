1a2e5445e58e3e365ff45d892969170ad269a90a
jhdoubler01 <jhdoubler01@gmail.com>
2024-04-13 18:38:03 -0400

    level 5

--- Assets/Scripts/Common/GlobalLibrary.cs
+++ Assets/Scripts/Common/GlobalLibrary.cs
@@ -6,7 +6,7 @@ public class GlobalLibrary
 {
     public static readonly Dictionary<SceneCategory, SceneTitle[]> G_SCENE_CATEGORY_DICT = new Dictionary<SceneCategory, SceneTitle[]>
     {
-        {SceneCategory.All, new SceneTitle[]{SceneTitle.MainMenu, SceneTitle.Level1, SceneTitle.Level2, SceneTitle.Level3, SceneTitle.Level4} },
+        {SceneCategory.All, new SceneTitle[]{SceneTitle.MainMenu, SceneTitle.Level0, SceneTitle.Level1, SceneTitle.Level2, SceneTitle.Level3, SceneTitle.Level4, SceneTitle.Level5, SceneTitle.Level6, SceneTitle.Level7, SceneTitle.Level8, SceneTitle.Level9} },
     };
 
 }
@@ -17,8 +17,14 @@ public enum SceneCategory
 public enum SceneTitle
 {
     MainMenu,
+    Level0,
     Level1,
     Level2,
     Level3,
     Level4,
+    Level5,
+    Level6,
+    Level7,
+    Level8,
+    Level9,
 }
