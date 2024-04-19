84c88f32c56126eb51d15ab6150d0d38cac923c4
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2024-04-18 21:14:25 -0400

    level select

--- Assets/Scripts/MainMenu/LevelButton.cs
+++ Assets/Scripts/MainMenu/LevelButton.cs
@@ -9,6 +9,7 @@ public class LevelButton : MonoBehaviour
     public SceneTitle scene;
 
     [SerializeField] public Button button;
+    [SerializeField] GameObject locked, unlocked, inProgress;
     public void OnSelect()
     {
         SKAudioManager.instance.PlaySound("UI3");
